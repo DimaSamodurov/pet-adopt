f1dd2de15abaf9f76c5919c23f3a2017caf18c97
Dima Samodurov <dimasamodurov@gmail.com>
2015-02-20 06:38:54 +0200

    Automatically extract data from old pages.

---
+++ lib/sample/scrape.rb
@@ -0,0 +1,47 @@
+# This script extracts data from the html pages of the old site zoo.ratusha.lviv.ua to yaml files.
+# Pages had been saved to cats.html and dogs.html files.
+require 'nokogiri'
+require 'active_support/core_ext/array/grouping'
+require 'yaml'
+
+class PetScraper
+
+  def parse(file_name)
+    doc = Nokogiri::HTML(File.open("dogs.html"))
+    items = doc.xpath('html/body/div[1]/table[2]/tbody/tr/td[2]/div[4]/div').map &:text
+
+    pets = items.to_a.in_groups_of 4
+    pets.map{|pet| to_hash pet }
+  end
+
+  private
+
+  def to_hash(pet_array)
+    {
+        name: pet_array[0],
+        description: pet_array[1],
+        post_date: parse_post_date(pet_array[2]),
+        publishing_date: parse_publishing_date(pet_array[2])
+    }
+  end
+
+  def parse_post_date(string)
+    Date.strptime /На прилаштування (\d\d\.\d\d\.\d\d)/.match(string)[1], '%d.%m.%y'
+  end
+
+  def parse_publishing_date(string)
+    Date.strptime /Опубліковано: (\d\d\.\d\d\.\d\d)/.match(string)[1], '%d.%m.%y'
+  end
+end
+
+scraper = PetScraper.new
+
+dogs = scraper.parse 'dogs.html'
+File.write 'dogs.yaml', dogs.to_yaml
+puts 'Dogs extracted to dogs.yaml'
+
+cats = scraper.parse 'cats.html'
+File.write 'cats.yaml', cats.to_yaml
+puts 'Cats extracted to dogs.yaml'
+
+
