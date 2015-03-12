6cb2a008c1dd5766ec9f3f0ea19ed518ed2273d2
Dima Samodurov <dmitriy.samodurov@sap.com>
2015-03-12 04:19:37 +0200

    Extend Pet model with species and breed.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,6 +4,9 @@ class Pet
   field :name,          type: String
   field :description,   type: String
   field :state,         type: String
+  field :species,       type: String
+  field :breed,         type: String
+  field :year_of_birth, type: Integer
 
   attachment :picture
 end
