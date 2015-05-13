8023e304a8697205aede41b61c89bbb4d2b484c4
Lyubomyr Kardash <Kardash.Lviv@gmail.com>
2015-05-13 17:00:40 +0300

    add stylesheets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
 //= require turbolinks
-//= require dropzone.min
+//= require foundation
 //= require_tree .
-
-$(function(){ $(document).foundation(); });
+$(function() {
+    $(document).foundation();
+});
